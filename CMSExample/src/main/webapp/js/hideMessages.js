21a43815044fc8edd1b8fc127ee4ca95ed2780d2
Stefan Bradl <stefan_bradl@web.de>
2012-02-10 22:53:29 +0100

    -

--- CMSExample/src/main/webapp/js/hideMessages.js
+++
@@ -1,6 +0,0 @@
-var liftMessages = document.getElementById("lift__noticesContainer__");
-
-if (!liftMessages.hasChildNodes()) {
-	var msgs = document.getElementById("messages");
-	msgs.style.display = "none";
-}
