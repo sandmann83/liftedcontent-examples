9fdf9ac952caa04d45180359680ae84bf684f39c
Stefan Bradl <stefan_bradl@web.de>
2012-03-03 17:14:22 +0100

    -

--- BlogExample/src/main/scala/bootstrap/liftweb/Boot.scala
+++ BlogExample/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -1,11 +1,14 @@
 package bootstrap.liftweb
 
+import eu.sbradl.liftedcontent.blog.BlogModule
+
 import net.liftweb.common.Empty
 import net.liftweb.common.Full
+import net.liftweb.http.LiftRulesMocker.toLiftRules
 import net.liftweb.http.LiftRules
 import net.liftweb.http.NoticeType
-import net.liftweb.util.Helpers._
-import eu.sbradl.liftedcontent.blog.BlogModule
+import net.liftweb.util.Helpers.intToTimeSpanBuilder
+import net.liftweb.util.Vendor.valToVender
 
 class Boot extends eu.sbradl.liftedcontent.core.Boot {
 
